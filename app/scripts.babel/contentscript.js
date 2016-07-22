cee3d3b9eb11b9f05416eb19187854daa8789518
anuragagarwal561994 <anurag.agarwal561994@gmail.com>
2016-07-22 14:35:47 +0530

    Implements imdb rating alongside movie name using OMDB API

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -3,6 +3,18 @@
 (function() {
   var shows = document.getElementsByClassName('caption');
   for (var i = 0, length = shows.length; i < length; i++) {
-    console.log(shows[i].innerText);
+    fetchData(shows[i], shows[i].innerText);
+  }
+
+  function fetchData(element, title) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', `http://www.omdbapi.com/?t=${title}`, true);
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        var response = JSON.parse(xhr.responseText);
+        element.innerText += ` (IMDB: ${response.imdbRating} / 10)`;
+      }
+    };
+    xhr.send();
   }
 })();
