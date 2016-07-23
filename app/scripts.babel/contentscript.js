484245bc8ce69043ccd91f4d368fc1870f0667f3
anuragagarwal561994 <anurag.agarwal561994@gmail.com>
2016-07-23 15:57:52 +0530

    Adds popup element with extra show information

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -23,5 +23,73 @@
 
   function renderElement(element, response) {
     element.innerText += ` (IMDB: ${response.imdbRating} / 10)`;
+
+    element.onmouseover = (e) => {
+      var bodyElement = document.body;
+      var popupElement = popUp(response);
+      var elementPosition = getPosition(e.currentTarget);
+      var documentElement = document.documentElement || document.body.parentNode || document.body;
+      var scrollTop = window.pageYOffset || documentElement.scrollTop;
+
+      popupElement.style.display = 'block';
+      popupElement.style.top = `${elementPosition.y + scrollTop + element.offsetHeight + 10}px`;
+      popupElement.style.left = `${elementPosition.x}px`;
+      bodyElement.appendChild(popupElement);
+    };
+    element.onmouseout = () => {
+      document.body.removeChild(document.getElementById('mdb_balloon'));
+    };
+  }
+
+  function popUp(response){
+    var popup = document.createElement('div');
+    var contentElement = document.createElement('div');
+    var movieDetails = document.createElement('div');
+
+    popup.id = 'mdb_balloon';
+    contentElement.id = 'mdb_balloon_content';
+
+    movieDetails.id = 'mdb_balloon_details';
+    movieDetails.appendChild(popupTextElement(`<b>Actors</b>: ${response.Actors}`));
+    movieDetails.appendChild(popupTextElement(`<b>Awards</b>: ${response.Awards}`));
+    movieDetails.appendChild(popupTextElement(`<b>Genre</b>: ${response.Genre}`));
+    movieDetails.appendChild(popupTextElement(`${response.Plot}`));
+    contentElement.appendChild(movieDetails);
+
+    popup.appendChild(contentElement);
+    return popup;
+  }
+
+  function popupTextElement(html) {
+    var element = document.createElement('div');
+    element.innerHTML = html;
+    return element;
+  }
+
+  // Helper function to get an element's exact position
+  function getPosition(el) {
+    var xPos = 0;
+    var yPos = 0;
+
+    while (el) {
+      if (el.tagName == 'BODY') {
+        // deal with browser quirks with body/window/document and page scroll
+        var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+        var yScroll = el.scrollTop || document.documentElement.scrollTop;
+
+        xPos += (el.offsetLeft - xScroll + el.clientLeft);
+        yPos += (el.offsetTop - yScroll + el.clientTop);
+      } else {
+        // for all other non-BODY elements
+        xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+        yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+      }
+
+      el = el.offsetParent;
+    }
+    return {
+      x: xPos,
+      y: yPos
+    };
   }
 })();
